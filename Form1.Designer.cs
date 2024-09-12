810298e6498d49f961b1c866d6cac7a96fa26c7d
juank996 <jc11r@hotmail.com>
2024-09-12 15:01:56 -0300

    1500

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.ComponentModel;
-using System.Drawing;
-using System.Windows.Forms;
-
-
-namespace GUI_JAM
+﻿namespace GUI_JAM
 {
     partial class Form1
     {
@@ -21,17 +15,156 @@ namespace GUI_JAM
 
         private void InitializeComponent()
         {
+            this.buttonLimpiar = new System.Windows.Forms.Button();
+            this.buttonExportar = new System.Windows.Forms.Button();
+            this.textBoxFsoNumber = new System.Windows.Forms.TextBox();
+            this.textBoxDescripcionTrabajo = new System.Windows.Forms.TextBox();
+            this.textBoxCliente = new System.Windows.Forms.TextBox();
+            this.textBoxWell = new System.Windows.Forms.TextBox();
+            this.textBoxRepresentante = new System.Windows.Forms.TextBox();
+            this.dateTimePickerFecha = new System.Windows.Forms.DateTimePicker();
+            this.timePickerHora = new System.Windows.Forms.DateTimePicker();
+            this.textBoxRig = new System.Windows.Forms.TextBox();
+            this.textBoxCreadoPor = new System.Windows.Forms.TextBox();
+            this.textBoxTecnico1 = new System.Windows.Forms.TextBox();
+            this.textBoxTecnico2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // buttonLimpiar
+            // 
+            this.buttonLimpiar.Location = new System.Drawing.Point(12, 12);
+            this.buttonLimpiar.Name = "buttonLimpiar";
+            this.buttonLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.buttonLimpiar.TabIndex = 0;
+            this.buttonLimpiar.Text = "Limpiar";
+            this.buttonLimpiar.UseVisualStyleBackColor = true;
+            this.buttonLimpiar.Click += new System.EventHandler(this.buttonLimpiar_Click);
+            // 
+            // buttonExportar
+            // 
+            this.buttonExportar.Location = new System.Drawing.Point(93, 12);
+            this.buttonExportar.Name = "buttonExportar";
+            this.buttonExportar.Size = new System.Drawing.Size(75, 23);
+            this.buttonExportar.TabIndex = 1;
+            this.buttonExportar.Text = "Exportar";
+            this.buttonExportar.UseVisualStyleBackColor = true;
+            this.buttonExportar.Click += new System.EventHandler(this.buttonExportar_Click);
+            // 
+            // textBoxFsoNumber
+            // 
+            this.textBoxFsoNumber.Location = new System.Drawing.Point(12, 41);
+            this.textBoxFsoNumber.Name = "textBoxFsoNumber";
+            this.textBoxFsoNumber.Size = new System.Drawing.Size(156, 23);
+            this.textBoxFsoNumber.TabIndex = 2;
+            // 
+            // textBoxDescripcionTrabajo
+            // 
+            this.textBoxDescripcionTrabajo.Location = new System.Drawing.Point(12, 70);
+            this.textBoxDescripcionTrabajo.Name = "textBoxDescripcionTrabajo";
+            this.textBoxDescripcionTrabajo.Size = new System.Drawing.Size(156, 23);
+            this.textBoxDescripcionTrabajo.TabIndex = 3;
+            // 
+            // textBoxCliente
+            // 
+            this.textBoxCliente.Location = new System.Drawing.Point(12, 99);
+            this.textBoxCliente.Name = "textBoxCliente";
+            this.textBoxCliente.Size = new System.Drawing.Size(156, 23);
+            this.textBoxCliente.TabIndex = 4;
+            // 
+            // textBoxWell
+            // 
+            this.textBoxWell.Location = new System.Drawing.Point(12, 128);
+            this.textBoxWell.Name = "textBoxWell";
+            this.textBoxWell.Size = new System.Drawing.Size(156, 23);
+            this.textBoxWell.TabIndex = 5;
+            // 
+            // textBoxRepresentante
+            // 
+            this.textBoxRepresentante.Location = new System.Drawing.Point(12, 157);
+            this.textBoxRepresentante.Name = "textBoxRepresentante";
+            this.textBoxRepresentante.Size = new System.Drawing.Size(156, 23);
+            this.textBoxRepresentante.TabIndex = 6;
+            // 
+            // dateTimePickerFecha
+            // 
+            this.dateTimePickerFecha.Location = new System.Drawing.Point(12, 186);
+            this.dateTimePickerFecha.Name = "dateTimePickerFecha";
+            this.dateTimePickerFecha.Size = new System.Drawing.Size(156, 23);
+            this.dateTimePickerFecha.TabIndex = 7;
+            // 
+            // timePickerHora
+            // 
+            this.timePickerHora.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.timePickerHora.Location = new System.Drawing.Point(12, 215);
+            this.timePickerHora.Name = "timePickerHora";
+            this.timePickerHora.Size = new System.Drawing.Size(156, 23);
+            this.timePickerHora.TabIndex = 8;
+            // 
+            // textBoxRig
+            // 
+            this.textBoxRig.Location = new System.Drawing.Point(12, 244);
+            this.textBoxRig.Name = "textBoxRig";
+            this.textBoxRig.Size = new System.Drawing.Size(156, 23);
+            this.textBoxRig.TabIndex = 9;
+            // 
+            // textBoxCreadoPor
+            // 
+            this.textBoxCreadoPor.Location = new System.Drawing.Point(12, 273);
+            this.textBoxCreadoPor.Name = "textBoxCreadoPor";
+            this.textBoxCreadoPor.Size = new System.Drawing.Size(156, 23);
+            this.textBoxCreadoPor.TabIndex = 10;
+            // 
+            // textBoxTecnico1
+            // 
+            this.textBoxTecnico1.Location = new System.Drawing.Point(12, 302);
+            this.textBoxTecnico1.Name = "textBoxTecnico1";
+            this.textBoxTecnico1.Size = new System.Drawing.Size(156, 23);
+            this.textBoxTecnico1.TabIndex = 11;
+            // 
+            // textBoxTecnico2
+            // 
+            this.textBoxTecnico2.Location = new System.Drawing.Point(12, 331);
+            this.textBoxTecnico2.Name = "textBoxTecnico2";
+            this.textBoxTecnico2.Size = new System.Drawing.Size(156, 23);
+            this.textBoxTecnico2.TabIndex = 12;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(284, 361);
+            this.Controls.Add(this.textBoxTecnico2);
+            this.Controls.Add(this.textBoxTecnico1);
+            this.Controls.Add(this.textBoxCreadoPor);
+            this.Controls.Add(this.textBoxRig);
+            this.Controls.Add(this.timePickerHora);
+            this.Controls.Add(this.dateTimePickerFecha);
+            this.Controls.Add(this.textBoxRepresentante);
+            this.Controls.Add(this.textBoxWell);
+            this.Controls.Add(this.textBoxCliente);
+            this.Controls.Add(this.textBoxDescripcionTrabajo);
+            this.Controls.Add(this.textBoxFsoNumber);
+            this.Controls.Add(this.buttonExportar);
+            this.Controls.Add(this.buttonLimpiar);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Formulario";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
         }
+
+        private System.Windows.Forms.Button buttonLimpiar;
+        private System.Windows.Forms.Button buttonExportar;
+        private System.Windows.Forms.TextBox textBoxFsoNumber;
+        private System.Windows.Forms.TextBox textBoxDescripcionTrabajo;
+        private System.Windows.Forms.TextBox textBoxCliente;
+        private System.Windows.Forms.TextBox textBoxWell;
+        private System.Windows.Forms.TextBox textBoxRepresentante;
+        private System.Windows.Forms.DateTimePicker dateTimePickerFecha;
+        private System.Windows.Forms.DateTimePicker timePickerHora;
+        private System.Windows.Forms.TextBox textBoxRig;
+        private System.Windows.Forms.TextBox textBoxCreadoPor;
+        private System.Windows.Forms.TextBox textBoxTecnico1;
+        private System.Windows.Forms.TextBox textBoxTecnico2;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,7 +1,7 @@
 using System;
-using System.Drawing;
-using System.ComponentModel;
 using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
 
 namespace GUI_JAM
 {
@@ -14,243 +14,72 @@ namespace GUI_JAM
 
         private void Form1_Load(object sender, EventArgs e)
         {
-          // Declaración de componentes
-          private ComboBox cbCabezal, cbCliente, cbLocacion, cbCreadoPor;
-          private TextBox txtRIG, txtWELL, txtHora, txtTecnicos, txtDescripcion, txtRepresentante, txtFSO;
-          private Label lblPlantillaStatus;
-          private PictureBox pbPreview;
-          private DateTimePicker dtpFecha;
-          private Button btnLimpiar, btnExportar;
-          private CheckBox cbDarkMode, cbSoloCaratula;
-        }
-        public MainForm()
-        {
-            InitializeComponent();
-            this.Size = new Size(1280, 720);
-            this.MinimumSize = new Size(1280, 720);
-            this.MaximumSize = new Size(1920, 1080);
-            this.Text = "GUI JAM - Llenado de Plantillas";
-            this.BackColor = Color.FromArgb(45, 45, 48);  // Dark mode por defecto
-
-            InitializeUI();
+            // Código adicional para inicializar el formulario, si es necesario
         }
 
-        private void InitializeUI()
+        private void buttonLimpiar_Click(object sender, EventArgs e)
         {
-            // Texto "Caract. Especiales" con los switches
-            Label lblCaractEspeciales = new Label()
-            {
-                Text = "Caract. Especiales",
-                Location = new Point(20, 20),
-                AutoSize = true,
-                ForeColor = Color.White
-            };
-            this.Controls.Add(lblCaractEspeciales);
-
-            // Switch de Dark/Light Mode
-            cbDarkMode = new CheckBox()
-            {
-                Text = "Dark",
-                Location = new Point(150, 20),
-                ForeColor = Color.White,
-                AutoSize = true,
-                Checked = true
-            };
-            cbDarkMode.CheckedChanged += new EventHandler(ToggleDarkMode);
-            this.Controls.Add(cbDarkMode);
-
-            // Switch "Solo Caratula"
-            cbSoloCaratula = new CheckBox()
-            {
-                Text = "Solo Carátula",
-                Location = new Point(250, 20),
-                ForeColor = Color.White,
-                AutoSize = true
-            };
-            this.Controls.Add(cbSoloCaratula);
-
-            // Texto "Tipo de cabezal"
-            Label lblTipoCabezal = new Label()
-            {
-                Text = "Tipo de cabezal",
-                Location = new Point(20, 60),
-                AutoSize = true,
-                ForeColor = Color.White
-            };
-            this.Controls.Add(lblTipoCabezal);
-
-            // ComboBox para cabezal
-            cbCabezal = new ComboBox()
-            {
-                Location = new Point(150, 60),
-                Width = 200
-            };
-            cbCabezal.Items.AddRange(new string[] { "Cabezal Hibrido", "Cabezal Robusto", "Cabezal CFB", "Cabezal HES", "BPV.Lub", "BPV.Lub Seccion C", "BPV.Lub Seccion C & T" });
-            this.Controls.Add(cbCabezal);
-
-            // Label para el estado de la plantilla
-            lblPlantillaStatus = new Label()
-            {
-                Text = "Plantilla no disponible",
-                Location = new Point(20, 100),
-                AutoSize = true,
-                ForeColor = Color.White,
-                BackColor = Color.Red,
-                Width = 300
-            };
-            this.Controls.Add(lblPlantillaStatus);
-
-            // PictureBox para la vista previa
-            pbPreview = new PictureBox()
-            {
-                Location = new Point(400, 20),
-                Size = new Size(200, 200),
-                ImageLocation = "C:/Users/jc11r/Desktop/GUI_JAM/previews/Empty.jpg",
-                SizeMode = PictureBoxSizeMode.StretchImage
-            };
-            this.Controls.Add(pbPreview);
-
-            // Comboboxes y Entryboxes
-            CreateLabeledTextBox("Cliente", ref cbCliente, new string[] { "", "YPF", "SHELL" }, 20, 140);
-            CreateLabeledTextBox("RIG", ref txtRIG, 20, 180);
-            CreateLabeledTextBox("WELL", ref txtWELL, 20, 220);
-
-            // DateTimePicker para la fecha solicitada
-            Label lblFechaSolicitada = new Label()
-            {
-                Text = "Fecha solicitada",
-                Location = new Point(20, 260),
-                AutoSize = true,
-                ForeColor = Color.White
-            };
-            this.Controls.Add(lblFechaSolicitada);
-            dtpFecha = new DateTimePicker()
-            {
-                Format = DateTimePickerFormat.Short,
-                Location = new Point(150, 260)
-            };
-            this.Controls.Add(dtpFecha);
-
-            // EntryBox para la hora solicitada
-            CreateLabeledTextBox("Hora solicitada", ref txtHora, 20, 300);
-
-            // TextBox para los técnicos
-            CreateLabeledTextBox("Tecnicos", ref txtTecnicos, 20, 340);
-
-            // TextBox para la descripción del trabajo
-            CreateLabeledTextBox("Descripción del trabajo", ref txtDescripcion, 20, 380, width: 300, multiline: true);
-
-            // TextBox para el representante
-            CreateLabeledTextBox("Representante", ref txtRepresentante, 20, 460);
-
-            // Combobox para locación
-            CreateLabeledTextBox("Locación", ref cbLocacion, new string[] { "", "Añelo", "Río Neuquén" }, 20, 500);
-
-            // EntryBox para FSO
-            Label lblFSO = new Label()
-            {
-                Text = "FSO",
-                Location = new Point(20, 540),
-                AutoSize = true,
-                ForeColor = Color.White
-            };
-            this.Controls.Add(lblFSO);
-            Label lblFSOPrefix = new Label()
-            {
-                Text = "000-",
-                Location = new Point(150, 540),
-                AutoSize = true,
-                ForeColor = Color.White
-            };
-            this.Controls.Add(lblFSOPrefix);
-            txtFSO = new TextBox()
-            {
-                Location = new Point(190, 540),
-                Width = 60,
-                MaxLength = 4
-            };
-            this.Controls.Add(txtFSO);
-
-            // Combobox para "Creado por"
-            CreateLabeledTextBox("Creado por", ref cbCreadoPor, new string[] { "", "Balacero", "Bart S", "Moledor" }, 20, 580);
-
-            // Botón Limpiar
-            btnLimpiar = new Button()
-            {
-                Text = "Limpiar",
-                Location = new Point(400, 600),
-                Width = 100
-            };
-            btnLimpiar.Click += BtnLimpiar_Click;
-            this.Controls.Add(btnLimpiar);
-
-            // Botón Exportar
-            btnExportar = new Button()
-            {
-                Text = "Exportar",
-                Location = new Point(520, 600),
-                Width = 100
-            };
-            btnExportar.Click += BtnExportar_Click;
-            this.Controls.Add(btnExportar);
+            // Código para limpiar los campos del formulario
+            textBoxFsoNumber.Clear();
+            textBoxDescripcionTrabajo.Clear();
+            textBoxCliente.Clear();
+            textBoxWell.Clear();
+            textBoxRepresentante.Clear();
+            dateTimePickerFecha.Value = DateTime.Now;
+            timePickerHora.Value = DateTime.Now;
+            textBoxRig.Clear();
+            textBoxCreadoPor.Clear();
+            textBoxTecnico1.Clear();
+            textBoxTecnico2.Clear();
         }
 
-        // Método para crear etiquetas y textboxes
-        private void CreateLabeledTextBox(string labelText, ref TextBox textBox, int x, int y, int width = 200, bool multiline = false)
+        private void buttonExportar_Click(object sender, EventArgs e)
         {
-            Label label = new Label()
-            {
-                Text = labelText,
-                Location = new Point(x, y),
-                AutoSize = true,
-                ForeColor = Color.White
-            };
-            this.Controls.Add(label);
+            // Código para exportar datos a Excel y PDF
+            // Aquí deberás implementar la lógica específica para exportar los datos.
+            string fsoNumber = textBoxFsoNumber.Text;
+            string descripcionTrabajo = textBoxDescripcionTrabajo.Text;
+            string cliente = textBoxCliente.Text;
+            string well = textBoxWell.Text;
+            string representante = textBoxRepresentante.Text;
+            string fecha = dateTimePickerFecha.Value.ToString("yyyy-MM-dd");
+            string hora = timePickerHora.Value.ToString("HH:mm");
+            string rig = textBoxRig.Text;
+            string creadoPor = textBoxCreadoPor.Text;
+            string tecnico1 = textBoxTecnico1.Text;
+            string tecnico2 = textBoxTecnico2.Text;
 
-            textBox = new TextBox()
-            {
-                Location = new Point(150, y),
-                Width = width,
-                Multiline = multiline
-            };
-            this.Controls.Add(textBox);
+            // Aquí deberás implementar la lógica para crear y llenar el archivo Excel
+            // y luego convertirlo a PDF. Usa las librerías adecuadas que has instalado.
+
+            // Ejemplo (deberás ajustar según tu lógica):
+            // string excelPath = ExportarAExcel(fsoNumber, descripcionTrabajo, cliente, well, representante, fecha, hora, rig, creadoPor, tecnico1, tecnico2);
+            // ExportarAPDF(excelPath);
         }
 
-        // Método para crear etiquetas y comboboxes
-        private void CreateLabeledTextBox(string labelText, ref ComboBox comboBox, string[] items, int x, int y)
+        protected override void Dispose(bool disposing)
         {
-            Label label = new Label()
+            if (disposing && (components != null))
             {
-                Text = labelText,
-                Location = new Point(x, y),
-                AutoSize = true,
-                ForeColor = Color.White
-            };
-            this.Controls.Add(label);
-
-            comboBox = new ComboBox()
-            {
-                Location = new Point(150, y),
-                Width = 200
-            };
-            comboBox.Items.AddRange(items);
-            this.Controls.Add(comboBox);
+                components.Dispose();
+            }
+            base.Dispose(disposing);
         }
 
-        // Eventos de los botones
-        private void BtnLimpiar_Click(object sender, EventArgs e)
+        private System.ComponentModel.IContainer components = null;
+
+        // Métodos para exportar a Excel y PDF (deberás implementar estos métodos)
+        private string ExportarAExcel(string fsoNumber, string descripcionTrabajo, string cliente, string well, string representante, string fecha, string hora, string rig, string creadoPor, string tecnico1, string tecnico2)
         {
-            // Lógica para limpiar todos los campos
+            // Implementa aquí la lógica para exportar a Excel
+            // Esta función debe devolver la ruta del archivo Excel creado
+            throw new NotImplementedException();
         }
 
-        private void BtnExportar_Click(object sender, EventArgs e)
+        private void ExportarAPDF(string excelPath)
         {
-            // Lógica para exportar la información a Excel y PDF
-        }
-
-        private void ToggleDarkMode(object sender, EventArgs e)
-        {
-            // Lógica para alternar entre modo Dark y Light
+            // Implementa aquí la lógica para convertir el archivo Excel a PDF
+            throw new NotImplementedException();
         }
     }
 }
